047b8b2a149aa577d30e0b944f7bb72952e8aa84
guardiankiller <com_1998@abv.bg>
2024-12-10 17:56:32 +0200

    repo init

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.13.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "a402d8f1-cb6f-4c64-a62f-25f43b25cf57"
+  features {
+  }
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "mishorg" {
+  location = var.resource_group_location
+  name     = "${var.resource_group_name}-${random_integer.ri.result}"
+}
+
+resource "azurerm_service_plan" "mishoasp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.mishorg.name
+  location            = azurerm_resource_group.mishorg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "${var.app_service_name}${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.mishorg.name
+  location            = azurerm_service_plan.mishoasp.location
+  service_plan_id     = azurerm_service_plan.mishoasp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlservermisho.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.databasemisho.name};User ID=${azurerm_mssql_server.sqlservermisho.administrator_login};Password=${azurerm_mssql_server.sqlservermisho.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_mssql_server" "sqlservermisho" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.mishorg.name
+  location                     = azurerm_resource_group.mishorg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_user
+  administrator_login_password = var.sql_user_pass
+}
+
+resource "azurerm_mssql_database" "databasemisho" {
+  name           = var.sql_database_name
+  server_id      = azurerm_mssql_server.sqlservermisho.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "firewallmisho" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.sqlservermisho.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "github" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.github_repo
+  branch                 = "main"
+  use_manual_integration = true
+}
